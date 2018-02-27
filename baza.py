bd6c3449185cd084e00d4e4b0e770f2a57625e27
Maks Kolman <kolman.maks@gmail.com>
2018-02-27 11:33:25 +0100

    Migrate to postgresql

--- baza.py
+++ baza.py
@@ -1,14 +1,14 @@
-import sqlite3
-
 import os
 from urllib import parse
 import psycopg2
 
-parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])
+DATABASE_URL = "postgres://postgres:geslo@localhost:5432/vislice"
 
 
 def naredi_povezavo():
+    """ Naredi in vrne povezavo do baze podatkov """
+    parse.uses_netloc.append("postgres")
+    url = parse.urlparse(os.environ.get("DATABASE_URL", DATABASE_URL))
     return psycopg2.connect(
         database=url.path[1:],
         user=url.username,
@@ -18,14 +18,36 @@ def naredi_povezavo():
     )
 
 
+def zakodiraj_geslo(password):
+    """ Zakodira geslo in ga vrne """
+    import hashlib, binascii
+    dk = hashlib.pbkdf2_hmac('sha256', password.encode(), b'salt', 100000)
+    return binascii.hexlify(dk).decode()
+
+
 def ustvari_tabele():
-    """ Naredi dve tabeli: Users in Scores. """
-    # povezava = sqlite3.connect(IME_BAZE)
+    """ Najprej izbriši in nato naredi dve tabeli: Users in Scores. """
     povezava = naredi_povezavo()
     kazalec = povezava.cursor()
-    kazalec.execute("CREATE TABLE Users (id int, username varchar(100))")
-    kazalec.execute(
-        "CREATE TABLE Scores (user_id int, napake int, beseda varchar(100))")
+
+    # Zbriši
+    kazalec.execute("DROP TABLE IF EXISTS Scores")
+    kazalec.execute("DROP TABLE IF EXISTS Users")
+
+    # Naredi tabelo Users
+    kazalec.execute("""CREATE TABLE Users (
+                           id       SERIAL PRIMARY KEY,
+                           username VARCHAR(100) UNIQUE NOT NULL,
+                           password VARCHAR(100) NOT NULL
+                       )""")
+    # Naredi tabelo Scores
+    kazalec.execute("""CREATE TABLE Scores (
+                           id       SERIAL PRIMARY KEY,
+                           user_id  INT,
+                           napake   INT,
+                           beseda   VARCHAR(100),
+                           FOREIGN KEY (user_id) REFERENCES Users (id)
+                       )""")
     povezava.commit()
     kazalec.close()
     povezava.close()
@@ -36,14 +58,37 @@ def napolni_tabele():
     from random import randint
     import uuid
     for i in range(10):
-        vstavi_novega_uporabnika(i, f"Uporabnik {i}")
+        user_id = vstavi_novega_uporabnika(f"Uporabnik {i}")
         for j in range(10):
-            vstavi_novo_igro(i, randint(20, 100), str(uuid.uuid4()))
+            vstavi_novo_igro(user_id, randint(30, 100), str(uuid.uuid4()))
+
+
+def vstavi_novega_uporabnika(username, password="123"):
+    povezava = naredi_povezavo()
+    kazalec = povezava.cursor()
+    kazalec.execute(
+        "INSERT INTO Users (username, password) VALUES (%s, %s) RETURNING id",
+        (username, zakodiraj_geslo(password)))
+    nov_user_id = kazalec.fetchone()[0]
+    povezava.commit()
+    kazalec.close()
+    povezava.close()
+    return nov_user_id
+
+
+def vstavi_novo_igro(user_id, napake, beseda):
+    """ Vstavi novo igro v tabelo Scores. """
+    povezava = naredi_povezavo()
+    kazalec = povezava.cursor()
+    kazalec.execute("""INSERT INTO Scores (user_id, napake, beseda)
+                       VALUES (%s, %s, %s)""", (user_id, napake, beseda))
+    povezava.commit()
+    kazalec.close()
+    povezava.close()
 
 
 def dobi_najboljse():
     """ Najdi 10 iger z najmanj napakami. """
-    # povezava = sqlite3.connect(IME_BAZE)
     povezava = naredi_povezavo()
     kazalec = povezava.cursor()
     kazalec.execute("""
@@ -55,29 +100,23 @@ def dobi_najboljse():
     return kazalec.fetchmany(10)
 
 
-def vstavi_novega_uporabnika(user_id, username):
-    # povezava = sqlite3.connect(IME_BAZE)
+def dobi_uporabnika(user_id=None, username=None, password=None):
+    """ V bazi najde in vrne uporabnika (če osbstaja) """
     povezava = naredi_povezavo()
     kazalec = povezava.cursor()
-    kazalec.execute(
-        "INSERT INTO Users VALUES (%s, %s)", (user_id, username))
-    povezava.commit()
-    kazalec.close()
-    povezava.close()
+    if user_id is not None:
+        kazalec.execute("SELECT * FROM Users WHERE id=%s", (user_id,))
+    elif username is not None and password is not None:
+        kazalec.execute("SELECT * FROM Users WHERE username=%s AND password=%s",
+                        (username, zakodiraj_geslo(password)))
+    elif username is not None:
+        kazalec.execute("SELECT * FROM Users WHERE username=%s", (username,))
+    else:
+        raise Exception("Napaka 'dobi_uporabnika': "
+                        "Nimam dovolj podatkov, da bi našel uporabnika.")
 
-
-def vstavi_novo_igro(user_id, napake, beseda):
-    """ Vstavi novo igro v tabelo Scores. """
-    # povezava = sqlite3.connect(IME_BAZE)
-    povezava = naredi_povezavo()
-    kazalec = povezava.cursor()
-    kazalec.execute(
-        "INSERT INTO Scores VALUES (%s, %s, %s)", (user_id, napake, beseda))
-    povezava.commit()
-    kazalec.close()
-    povezava.close()
+    return kazalec.fetchone()
 
 
 if __name__ == "__main__":
-    # ustvari_tabele()
-    napolni_tabele()
+    ustvari_tabele()
